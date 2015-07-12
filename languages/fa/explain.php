215b351e8bd6b1c1b3fcff5f43730dc8aa76ccf2
Glen Langer <contao.modul@glen-langer.de>
2015-07-12 16:09:49 +0200

    Update Transifex Language Files (fa)

--- languages/fa/explain.php
+++ languages/fa/explain.php
@@ -8,19 +8,18 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-02-09T06:50:54+01:00
+ * last-updated: 2015-02-07T21:13:33+01:00
  */
-
 
 $GLOBALS['TL_LANG']['XPL']['banner_help']['0']['0']  = 'نام بنر';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['1']['0']  = 'نشانی وب بنر';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['10']['0'] = 'صافی دامنه';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['2']['0']  = 'توزین بنر';
-$GLOBALS['TL_LANG']['XPL']['banner_help']['3']['0']  = 'تصویر بنر';
+$GLOBALS['TL_LANG']['XPL']['banner_help']['3']['0']  = 'پرونده بنر';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['4']['0']  = 'متن بنر';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['5']['0']  = 'مشاهده از (تاریخ و ساعت)';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['6']['0']  = 'مشاهده تا (تاریخ و ساعت)';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['7']['0']  = 'محدود کردن تعداد بازدید';
 $GLOBALS['TL_LANG']['XPL']['banner_help']['8']['0']  = 'محدود کردن تعداد کلیک‌ها';
-$GLOBALS['TL_LANG']['XPL']['banner_help']['9']['0']  = 'شناسه جزئی HTTP_USER_AGENT';
+$GLOBALS['TL_LANG']['XPL']['banner_help']['9']['0']  = 'صافی دامنه';
 
